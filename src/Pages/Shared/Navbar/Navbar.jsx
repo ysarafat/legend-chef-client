70ced5dce317a0ae3675aabad622f5847d69be90
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-05-01 22:58:48 +0600

    navbar add

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <nav>
-            <div>hello</div>
-            <div>hello</div>
+        <nav className="flex justify-between items-center container px-4 mx-auto py-5">
+            <div>
+                <h1 className="text-4xl font-bold ">Legend Chef</h1>
+            </div>
+            <ul className="flex items-center gap-4 text-base">
+                <li>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? 'text-primary' : 'text-[#161616] hover:text-red-500'
+                        }
+                    >
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/recipes"
+                        className={({ isActive }) =>
+                            isActive ? 'text-primary' : 'text-[#161616] hover:text-red-500'
+                        }
+                    >
+                        Recipes
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/login"
+                        className={({ isActive }) =>
+                            isActive ? 'text-primary' : 'text-[#161616] hover:text-red-500'
+                        }
+                    >
+                        Login
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/register"
+                        className={({ isActive }) =>
+                            isActive ? 'text-primary' : 'text-[#161616] hover:text-red-500'
+                        }
+                    >
+                        Register
+                    </NavLink>
+                </li>
+            </ul>
         </nav>
     );
 }
